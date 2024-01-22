027cac286c6730b11cb477a6f593afcfcde6b7fc
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2024-01-22 12:45:19 +0100

    Template Vite_React_RouterV6_Sass_Firebase

--- main-app/vite.config.ts
+++ main-app/vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig, loadEnv, splitVendorChunkPlugin} from 'vite';
+import Sitemap from 'vite-plugin-sitemap';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({command, mode}) => {
+    const paths = ['home'];
+    const dynamicRoutes = [
+        // ...paths.map((path) => `/gallery/${path}`),
+        // ...paths.map((path) => `/bg/${path}`),
+        // ...paths.map((path) => `/nl/${path}`),
+        ...paths.map((path) => `/${path}`),
+    ];
+
+    return {
+        plugins: [
+            splitVendorChunkPlugin(),
+            react(),
+            Sitemap({
+                // dynamicRoutes,
+                hostname: 'https://easy-letters.web.app',
+                readable: true,
+                robots: [
+                    {
+                        userAgent: '*',
+                        allow: '/',
+                    },
+                ],
+            }),
+        ],
+        build: {
+            minify: 'terser',
+            sourcemap: true,
+            manifest: true,
+
+            rollupOptions: {
+                output: {
+                    manualChunks(id: string) {
+                        // creating a chunk to @open-ish deps. Reducing the vendor chunk size
+                        if (id.includes('@open-ish') || id.includes('tslib')) {
+                            return '@open-ish';
+                        }
+                        // creating a chunk to react routes deps. Reducing the vendor chunk size
+                        if (
+                            id.includes('react-router-dom') ||
+                            id.includes('@remix-run') ||
+                            id.includes('react-router')
+                        ) {
+                            return '@react-router';
+                        }
+                    },
+                },
+            },
+        },
+    };
+});
